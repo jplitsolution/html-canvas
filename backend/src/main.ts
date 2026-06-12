2988aefba859ae0491e8878fa0ae4aa25dba66c1
gaurav012jais <gjaiswalk057@gmail.com>
2026-06-12 11:13:47 +0530

    update main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -19,7 +19,7 @@ async function bootstrap() {
 
   // Enable CORS
   app.enableCors({
-    origin: 'http://localhost:5173/',
+    origin: 'http://localhost:5173',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
